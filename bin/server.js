8343098fcd193454823bb2c2f711fad8b81ce95f
CarvalhoLC <carvalho.luisc@gmail.com>
2020-05-15 19:28:01 -0300

    Separando o Servidor

--- bin/server.js
+++ bin/server.js
@@ -2,24 +2,13 @@
 
 const http = require('http');
 const debug = require('debug')('nodestr:server');
-const express = require('express');
+const app = require('../src/app');
 
-const app = express();
 const port = normalizaPort(process.env.PORT || '3000'); // funciona com 3000 e com '3000'.
 
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
-
-app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);

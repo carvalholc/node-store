3c57924b94573095a1f3ef013f039bd5716506f9
CarvalhoLC <carvalho.luisc@gmail.com>
2020-05-15 20:52:42 -0300

    Controllers [ Produtos ]

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -2,21 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/product-controller');
 
-router.post('/', (req, res, next) => {
-    res.status(201).send(req.body); // 1' teste não retornou nada (devido express não sabe trabalhar com JSON - instalar body-parser)
-});
-
-router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body); // 1' teste não retornou nada (devido express não sabe trabalhar com JSON - instalar body-parser)
-});
+router.post('/', controller.post);
+router.put('/:id', controller.put);
+router.delete('/', controller.delete);
 
 module.exports = router;

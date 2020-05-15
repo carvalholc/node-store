b2e6bac620be69e7ab8ec221dce9de1b1717d6ce
CarvalhoLC <carvalho.luisc@gmail.com>
2020-05-15 20:14:58 -0300

    CRUD REST

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -12,6 +16,25 @@ const route = router.get('/', (req, res, next) => {
     });
 });
 
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body); // 1' teste não retornou nada (devido express não sabe trabalhar com JSON - instalar body-parser)
+});
+
+const alter = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
+const delet = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body); // 1' teste não retornou nada (devido express não sabe trabalhar com JSON - instalar body-parser)
+});
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', alter);
+app.use('/products', delet);
 
 module.exports = app;

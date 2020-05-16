f3d69afeaa3b336ed0d7248492e429c71dc593a6
CarvalhoLC <carvalho.luisc@gmail.com>
2020-05-16 09:23:08 -0300

    Listando um Produto pelo Id

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -4,31 +4,41 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
 exports.get = (req, res, next) => {
-    //    Product.find({})
-        Product.find({
-                    active: true
-                }, 'title slug price')
-               .then(data => {
-                    res.status(200).send(data);
-                }).catch(e => {
-                    res.status(400).send(e);
-                });
+//    Product.find({})
+    Product.find({
+                active: true
+            }, 'title slug price')
+            .then(data => {
+                res.status(200).send(data);
+            }).catch(e => {
+                res.status(400).send(e);
+            });
     };
     
 exports.getBySlug = (req, res, next) => {
-//        Product.find({  // Retorna um array. Utilizar "findOne".
-        Product.findOne({
-            slug: req.params.slug,
-                    active: true
-                }, 'title description slug price tags')
-                .then(data => {
-                    res.status(200).send(data);
-                }).catch(e => {
-                    res.status(400).send(e);
-                });
+//    Product.find({  // Retorna um array. Utilizar "findOne".
+    Product.findOne({
+        slug: req.params.slug,
+                active: true
+            }, 'title description slug price tags')
+            .then(data => {
+                res.status(200).send(data);
+            }).catch(e => {
+                res.status(400).send(e);
+            });
+    };
+
+exports.getById = (req, res, next) => {
+    Product.findById(req.params.id)
+            .then(data => {
+                res.status(200).send(data);
+            }).catch(e => {
+                res.status(400).send(e);
+            });
     };
         
-        exports.post = (req, res, next) => {
+
+exports.post = (req, res, next) => {
     // Opção 1.
     let product = new Product(req.body);
     product.save()
@@ -60,7 +70,6 @@ exports.getBySlug = (req, res, next) => {
             });
         });
 */
-
 };
 
 exports.put = (req, res, next) => {

fdd0acab128d9559a2454948aac9981d4f806aaa
CarvalhoLC <carvalho.luisc@gmail.com>
2020-05-16 08:52:21 -0300

    Listando os Produtos

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,6 +3,18 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
+exports.get = (req, res, next) => {
+//    Product.find({})
+    Product.find({
+                active: true
+            }, 'title slug price')
+           .then(data => {
+                res.status(200).send(data);
+            }).catch(e => {
+                res.status(400).send(e);
+            });
+};
+
 exports.post = (req, res, next) => {
     // Opção 1.
     let product = new Product(req.body);
@@ -23,7 +35,17 @@ exports.post = (req, res, next) => {
     product.title = req.body.title;
     product.slug = req.body.slug;
     // ...
-    product.save();
+    product.save()
+        .then(x => {
+            res.status(201).send({
+                message: 'Produto cadastrado com sucesso!'
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao cadastrar o produto!',
+                data: e
+            });
+        });
 */
 
 };

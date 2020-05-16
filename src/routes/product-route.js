c85ab8b4b19c51d3ce2935c12c7ae1d0b23d193f
CarvalhoLC <carvalho.luisc@gmail.com>
2020-05-16 19:28:21 -0300

    Excluindo um produto

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -11,6 +11,9 @@ router.get('/admin/:id', controller.getById);
 router.get('/tags/:tag', controller.getByTag);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
+// No delete podemos podemos passar o "id" como parametro da requisição. E no controller utilizar "req.params.id".
+//router.delete('/:id', controller.delete);
+// ou no delete podemos podemos passar o "id" no corpo da requisição. E no controller utilizar "req.body.id".
 router.delete('/', controller.delete);
 
 module.exports = router;

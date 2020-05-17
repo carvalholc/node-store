34cd43b8b966c48a2bf1ebff64b39064928b2513
CarvalhoLC <carvalho.luisc@gmail.com>
2020-05-17 20:15:14 -0300

    Revisitando os Controllers: Order - ajuste

--- src/repositories/order-repository.js
+++ src/repositories/order-repository.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 const Order = mongoose.model('Order');
 
 exports.get = async(data) => {
-    let res = await Order.find({});
+    //let res = await Order.find({}); // Não vem o "Customer" e o "Product" preenchido no GET.
+    let res = await Order.find({}, 'number status customer items')
+        .populate('customer', 'name')
+        .populate('items.product', 'title');
     return res;
 };
 

bada3157fd0a4708a298741c71e656a3936fb068
CarvalhoLC <carvalho.luisc@gmail.com>
2020-05-17 08:13:38 -0300

    Async/Await

--- src/repositories/product-repository.js
+++ src/repositories/product-repository.js
@@ -3,42 +3,48 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
-exports.get = () => {
+exports.get = async() => {
     //    Product.find({})
-    return Product
-        .find({
-            active: true
+    const res = await Product.find({
+        active: true
     }, 'title slug price');
+
+    return res;
 };
 
-exports.getBySlug = (slug) => {
-    return Product
+exports.getBySlug = async(slug) => {
+    const res = await Product
         .findOne({
             slug: slug,
             active: true
     }, 'title description slug price tags');
+
+    return res;
 };
 
-exports.getById = (id) => {
-    return Product
+exports.getById = async(id) => {
+    const res = await Product
         .findById(id);
+    
+        return res;
 };
 
-exports.getByTag = (tag) => {
-    return Product
-        .find({  // testar com "findOne" para um retorno, e para mais de um retorno. 
-            tags: tag,
-            active: true
-        }, 'title description slug price tags')
+exports.getByTag = async(tag) => {
+    const res = await Product.find({  // testar com "findOne" para um retorno, e para mais de um retorno. 
+        tags: tag,
+        active: true
+    }, 'title description slug price tags');
+
+    return res;
 };
 
-exports.create = (data) => {
+exports.create = async(data) => {
     let product = new Product(data);
-    return product.save();
+    await product.save();
 };
 
-exports.update = (id, data) => {
-    return Product
+exports.update = async(id, data) => {
+    await Product
         .findByIdAndUpdate(id, {
             $set: {
                 title: data.title,
@@ -49,7 +55,7 @@ exports.update = (id, data) => {
         });
 };
 
-exports.delete = (id) => {
-    return Product
+exports.delete = async(id) => {
+    await Product
         .findByIdAndRemove(id);
 };

27fd7909f11d18a97c59603b4d36e2bf8aff4b7f
CarvalhoLC <carvalho.luisc@gmail.com>
2020-05-17 07:04:10 -0300

    Repositórios

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,51 +3,46 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 const ValidationContract = require('../validators/fluent-validator');
+const repository = require('../repositories/product-repository');
 
 exports.get = (req, res, next) => {
-//    Product.find({})
-    Product.find({
-                active: true
-            }, 'title slug price')
-            .then(data => {
-                res.status(200).send(data);
-            }).catch(e => {
-                res.status(400).send(e);
-            });
+    repository
+        .get()
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
 };
     
 exports.getBySlug = (req, res, next) => {
-//    Product.find({  // Retorna um array. Utilizar "findOne".
-    Product.findOne({
-        slug: req.params.slug,
-                active: true
-            }, 'title description slug price tags')
-            .then(data => {
-                res.status(200).send(data);
-            }).catch(e => {
-                res.status(400).send(e);
-            });
+    repository
+        .getBySlug(req.params.slug)
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
 };
 
 exports.getById = (req, res, next) => {
-    Product.findById(req.params.id)
-            .then(data => {
-                res.status(200).send(data);
-            }).catch(e => {
-                res.status(400).send(e);
-            });
+    repository
+        .getById(req.params.id)
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
 };
         
 exports.getByTag = (req, res, next) => {
-    Product.find({  // testar com "findOne" para um retorno, e para mais de um retorno. 
-                tags: req.params.tag,
-                active: true
-            }, 'title description slug price tags')
-            .then(data => {
-                res.status(200).send(data);
-            }).catch(e => {
-                res.status(400).send(e);
-            });
+    repository
+        .getByTag(req.params.tag)
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
 };
             
 exports.post = (req, res, next) => {
@@ -63,8 +58,8 @@ exports.post = (req, res, next) => {
     };
 
     // Opção 1.
-    let product = new Product(req.body);
-    product.save()
+    repository
+        .create(req.body)
         .then(x => {
             res.status(201).send({
                 message: 'Produto cadastrado com sucesso!'
@@ -96,15 +91,9 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    Product
-        .findByIdAndUpdate(req.params.id, {
-            $set: {
-                title: req.body.title,
-                description: req.body.description,
-                price: req.body.price,
-                slug: req.body.slug
-            }
-        }).then(data => {
+    repository
+        .update(req.params.id, req.body)
+        .then(data => {
             res.status(200).send({
                 message: 'Produto atualizado com sucesso!'
             });
@@ -117,8 +106,8 @@ exports.put = (req, res, next) => {
 };
 
 exports.delete = (req, res, next) => {
-    Product
-        .findOneAndRemove(req.body.id)
+    repository
+        .delete(req.body.id)
         .then(data => {
             res.status(200).send({
                 message: 'Produto removido com sucesso!'
---
+++ src/repositories/product-repository.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product');
+
+exports.get = () => {
+    //    Product.find({})
+    return Product
+        .find({
+            active: true
+    }, 'title slug price');
+};
+
+exports.getBySlug = (slug) => {
+    return Product
+        .findOne({
+            slug: slug,
+            active: true
+    }, 'title description slug price tags');
+};
+
+exports.getById = (id) => {
+    return Product
+        .findById(id);
+};
+
+exports.getByTag = (tag) => {
+    return Product
+        .find({  // testar com "findOne" para um retorno, e para mais de um retorno. 
+            tags: tag,
+            active: true
+        }, 'title description slug price tags')
+};
+
+exports.create = (data) => {
+    let product = new Product(data);
+    return product.save();
+};
+
+exports.update = (id, data) => {
+    return Product
+        .findByIdAndUpdate(id, {
+            $set: {
+                title: data.title,
+                description: data.description,
+                price: data.price,
+                slug: data.slug
+            }
+        });
+};
+
+exports.delete = (id) => {
+    return Product
+        .findByIdAndRemove(id);
+};
